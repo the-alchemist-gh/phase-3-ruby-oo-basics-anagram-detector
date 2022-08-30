7451c77621df6f63d17232fc6e881e8168ab1eed
the-alchemist-gh <malikbanks01@gmail.com>
2022-08-30 11:04:32 +0000

    Completed lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+# require "pry"
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arrays)
+        new_array = arrays.filter do |el|
+            @word.chars.sort == el.chars.sort
+        end 
+        new_array
+    end
+end
+# listen = Anagram.new("listen")
+# listen.match(%w[enlists google inlets banana])
+# binding.pry
